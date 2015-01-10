5881d3353f44215a1c291c0b44c57e6bc0399992
rulison <rulison@berkeley.edu>
2015-01-10 00:01:22 -0800

    starting cs part

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 $(document).ready(function() {
 	
 
-	var firstExpanded = false;
-	var secondExpanded = false;
-	var thirdExpanded = false;
-
 	$("#site-about").mouseenter(function() {
-		console.log("secondexpanded");
-		secondExpanded = true;
 		$("#site-about-text").show(500);
-		if (firstExpanded && secondExpanded && thirdExpanded)
-			resizeBackground();
 	});
 	$("#me-about").mouseenter(function() {
-		firstExpanded = true;
-		console.log("firstexpanded");
 		$("#me-about-text").show(500);
-		if (firstExpanded && secondExpanded && thirdExpanded)
-			resizeBackground();
-	});
-	/*$("#site-about").mouseleave(function() {
-		$("#site-about-text").hide(500);
-	});*/
-	/*$("#me-about").mouseleave(function() {
-		$("#me-about-text").hide(500);
-	});*/
-	$("#contact").mouseenter(function() {
-		//thirdExpanded = true;
-		$("#contact-specifics").show(500);
-		if (firstExpanded && secondExpanded && thirdExpanded)
-			resizeBackground();
-	});
-	$( window ).resize(function() {
-	  console.log("resizing");
-	  $("#everything").css("height", "auto");
 	});
 
-	function resizeBackground() {
-		$("#everything").css("height", "200%");
-		$("#everything").css("width", "max");
-	}
+	$("#contact").mouseenter(function() {
+		$("#contact-specifics").show(500);
+	});
+
+
 });
