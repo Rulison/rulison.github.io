22234cfded21e2ba562affb393f0d495f918b599
MAC Book <MAC@calvisitor-10-105-167-6.calvisitor.1918.berkeley.edu>
2016-04-21 20:15:18 -0700

    add script

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("body").append("<p>Yes dude</p>");
+	$.ajax({
+  		url: "images",
+  		success: function(data){
+     		$(data).find("a:contains(.jpg)").each(function(){
+        	// will loop through 
+        	var images = $(this).attr("href");
+
+        	$('<p></p>').html(images).appendTo($("body"));
+
+     		});
+  		}	
+	});
+});
