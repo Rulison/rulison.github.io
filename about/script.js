3f5bdb46e7857fd09d5dbaf8150d88e3d9df7d92
MAC Book <MAC@MACmatoMacBook-Air.local>
2016-05-13 17:42:34 -0700

    about section

---
+++ about/script.js
@@ -0,0 +1,25 @@
+/*$(document).ready(function() {
+	$("body").append("<p>Yes dude</p>");
+	var jqxhr = $.get( "https://api.discogs.com/users/rulison/collection/folders/0/releases", function() {
+	  response = jQuery.parseJSON(jqxhr["responseText"]);
+	  releases = response["releases"];
+	  console.log(response);
+	  for(i = 0; i<releases.length; i++) {
+	  	release = releases[i];
+	  	p = document.createElement("p");
+	  	p.innerHTML = release["basic_information"]["title"];
+	  	$(p).appendTo($("body"));
+	  }
+	  console.log(releases[0]["basic_information"]["title"]);
+	})
+	  .done(function() {
+
+	  })
+	  .fail(function() {
+
+	  })
+	  .always(function() {
+
+	console.log(jqxhr);
+	  });
+});*/
