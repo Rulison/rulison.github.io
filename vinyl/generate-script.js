fe989bb8525e1b1abeb30347db52e8f68eee48bf
MAC Book <MAC@MACmatoMacBook-Air.local>
2016-05-13 21:54:34 -0700

    vinyl section

--- vinyl/generate-script.js
+++ vinyl/generate-script.js
@@ -1,24 +1,43 @@
+function processNext(index, maxIndex, releases, baseURL, keys) {
+  if (maxIndex == index) {
+    return;
+  }
+  single_response = $.get(baseURL+releases[index]["id"], function() {
+    response_json = single_response.responseJSON;
+    master_response = $.get(response_json["master_url"]+keys, function() {
+      img_src = master_response.responseJSON.images[0].resource_url;
+      div = document.createElement("div");
+      div.className = "frame three columns";
+      a = document.createElement("a");
+      a.href = response_json["uri"]
+      img = document.createElement("img");
+      img.src = img_src
+      img.className = "album"
+      $(a).appendTo(div);
+      $(img).appendTo(a);
+      $(div).appendTo($(".albums"));
+      processNext(index+1, maxIndex, releases, baseURL, keys);
+    });
+  });
+}
+
 $(document).ready(function() {
-	var jqxhr = $.get( "https://api.discogs.com/users/rulison/collection/folders/0/releases", function() {
-	  response = jQuery.parseJSON(jqxhr["responseText"]);
-	  releases = response["releases"];
-	  console.log(response);
-	  for(i = 0; i<releases.length; i++) {
-	  	release = releases[i];
-	  	p = document.createElement("p");
-	  	p.innerHTML = release["basic_information"]["title"];
-	  	$(p).appendTo($("body"));
-	  }
-	  console.log(releases[0]["basic_information"]["title"]);
-	})
-	  .done(function() {
+    //var collection = $.get( "https://api.discogs.com/users/rulison/collection/folders/0/releases", function() {
+    
+    var collection = $.ajax( {url:"https://api.discogs.com/users/rulison/collection/folders/0/releases?page=1&per_page=100",
+                             type: "get",
+                             data:{sort:"added", sort_order:"desc"}, success:function() {  
+      collection_json = collection.responseJSON;
+      releases = collection_json["releases"];
+      processNext(0, releases.length, releases, "https://api.discogs.com/releases/", keys);
+    }})
+    .done(function() {
 
-	  })
-	  .fail(function() {
+    })
+    .fail(function() {
 
-	  })
-	  .always(function() {
+    })
+    .always(function() {
 
-	console.log(jqxhr);
-	  });
+    });
 });
